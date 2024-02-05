c018ba4206817eb283c4267faa293a0e4fb64716
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2024-02-05 18:12:11 +0100

    config/container.php: Setup Clockwork services
    This allows retrieval of the Clockwork services from the DI container.
    Included is the XDebug profiler support and the file-based storage for
    the collected data.

--- config/container.php
+++ config/container.php
@@ -2,6 +2,11 @@
 
 declare(strict_types=1);
 
+use Clockwork\Clockwork;
+use Clockwork\DataSource\XdebugDataSource;
+use Clockwork\Storage\FileStorage;
+use Clockwork\Storage\StorageInterface;
+use Clockwork\Support\Vanilla\Clockwork as VanillaClockwork;
 use Laminas\ServiceManager\AbstractFactory\ReflectionBasedAbstractFactory;
 use Laminas\ServiceManager\ServiceManager;
 
@@ -13,6 +18,52 @@ $dependencies['services']['config'] = $config;
 $dependencies['abstract_factories'] = [
     ReflectionBasedAbstractFactory::class
 ];
+
+// register some services for Clockwork
+$dependencies['factories'][StorageInterface::class] = static function (ServiceManager $creationContext, string $resolvedName, ?array $options) {
+    return new FileStorage(__DIR__ . '/../
+    ');
+};
+$dependencies['factories'][VanillaClockwork::class] = static function (ServiceManager $creationContext, string $resolvedName, ?array $options) {
+    $res = new class extends VanillaClockwork
+    {
+        // TODO: All these methods here need to be integrated into the
+        // upstream VanillaClockwork.
+        public function isEnabled()
+        {
+            return $this->config['enable'];
+        }
+
+        public function getApiPath()
+        {
+            return $this->config['api'];
+        }
+
+        public function isWebEnabled()
+        {
+            return $this->config['web']['enable'];
+        }
+
+        public function getWebPath()
+        {
+            // TODO: Clarify what the meaning here is.
+            // return $this->config['web']['uri'];
+            return '/web';
+        }
+
+        public function isAuthenticationEnabled()
+        {
+            return $this->config['authentication'];
+        }
+    };
+    $res->getClockwork()->storage($creationContext->get(StorageInterface::class));
+    $res->getClockwork()->addDataSource($creationContext->get(XdebugDataSource::class));
+    return $res;
+};
+$dependencies['factories'][Clockwork::class] = static function (ServiceManager $creationContext, string $resolvedName, ?array $options) {
+    // just retrieve it from the helper, which unfortunately doesn't allow us to inject it
+    return $creationContext->get(VanillaClockwork::class)->getClockwork();
+};
 
 // Build container
 return new ServiceManager($dependencies);
