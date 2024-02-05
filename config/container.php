99f04dd21a5fa4835c9fccfc5a483f67b3f2644c
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2024-02-05 10:05:08 +0100

    Mezzio: Add reflection-based factory to framework configuration
    This allows creating DI services using just their class name.

--- config/container.php
+++ config/container.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Laminas\ServiceManager\AbstractFactory\ReflectionBasedAbstractFactory;
 use Laminas\ServiceManager\ServiceManager;
 
 // Load configuration
@@ -9,6 +10,9 @@ $config = require __DIR__ . '/config.php';
 
 $dependencies                       = $config['dependencies'];
 $dependencies['services']['config'] = $config;
+$dependencies['abstract_factories'] = [
+    ReflectionBasedAbstractFactory::class
+];
 
 // Build container
 return new ServiceManager($dependencies);
