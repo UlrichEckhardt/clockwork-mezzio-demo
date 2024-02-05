b4a574993fdef0c87894c7691ec88f88a38cb6e8
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2024-02-05 19:47:55 +0100

    public/index.php: deactivate some code
    It's unclear what this should do, possibly speed up retrieval of static
    files. However, it does interfere with the Clockwork web UI (/web).

--- public/index.php
+++ public/index.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 // Delegate static file requests back to the PHP built-in webserver
-if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__) {
-    return false;
-}
+// if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__) {
+//     return false;
+// }
 
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
